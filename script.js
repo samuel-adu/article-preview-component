55ec2c20e0118b1f116b4bf128cd6774881a498b
Samuel Adu <theadusamuel@gmail.com>
2022-07-13 14:30:08 +0100

    add functionality to share button

--- script.js
+++ script.js
@@ -2,6 +2,17 @@ const shareBtn = document.querySelector("#share-btn");
 const shareTab = document.querySelector("#share-tab");
 const card = document.querySelector("#card");
 
-shareBtn.addEventListener("click", function () {
+shareBtn.addEventListener("click", function (event) {
+  event.stopPropagation();
   shareTab.classList.add("share__tab--open");
+});
+
+shareTab.addEventListener("click", function (event) {
+  event.stopImmediatePropagation();
+});
+
+card.addEventListener("click", function () {
+  if (shareTab.classList.contains("share__tab--open")) {
+    shareTab.classList.remove("share__tab--open");
+  }
 });
